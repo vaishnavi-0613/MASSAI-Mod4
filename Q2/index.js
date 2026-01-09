0518b15fd2e588dbab089a70946a3c5b70bb918f
vaishnavi-0613 <vaishnavibeenaveni@gmail.com>
2026-01-09 18:26:36 +0530

    hii

---
+++ Q2/index.js
@@ -0,0 +1,9 @@
+// index.js
+
+import { checkPrime } from "./math.js";
+
+console.log(checkPrime(2));   // true
+console.log(checkPrime(5));   // true
+console.log(checkPrime(10));  // false
+console.log(checkPrime(17));  // true
+console.log(checkPrime(1));   // false
